9b982948dfd126e1474070fb51381ace9d9d3055
Hayes Choy <hayeselnut2127@gmail.com>
2020-08-18 00:32:11 +1000

    extracts metadata of playlist upon blur of txtbox

--- scripts/deduplicatify.js
+++ scripts/deduplicatify.js
@@ -17,7 +17,63 @@ $.ajax({
     }
 });
 
+function extract_playlist_id(playlist_link) {
+    // 2 cases
 
+    if (playlist_link.includes("open.spotify.com/playlist/")) {
+        // Playlist Link
+        var split = playlist_link.split("/")
+        return split[split.length - 1];
+    } else if (playlist_link.includes("spotify:playlist:")) {
+        // Spotify URL
+        return playlist_link.split(":")[2];
+    }
+}
+
+function update_playlist_metadata(name, owner, desc, img) {
+    $("#playlist-name").html(name);
+    $("#playlist-owner").html(owner);
+    $("#playlist-desc").html(desc);
+
+    console.log(img != null);
+
+    if (img != null) {
+        $("#playlist-img").attr("src", img[0].url);
+    }
+}
+
+// Consider using change or keydown event
+$("#dedup-txtbox").on("blur", function() {
+    var playlist_link = $(this).val();
+
+    var playlist_id = extract_playlist_id(playlist_link);
+
+    $.ajax({
+        type: "GET",
+        url: "https://api.spotify.com/v1/playlists/" + playlist_id,
+        headers: {"Authorization": "Bearer " + access_token},
+        success: function(response) {
+            console.log("NAME OF PLAYLIST: " + response.name);
+            console.log("OWNER: " + response.owner);
+            update_playlist_metadata(response.name, response.owner.display_name, response.description, response.images);
+        },
+        error: function() {
+            alert("Invalid Spotify playlist link");
+        }
+    });
+
+    // Check if playlist is correct $.ajax... etc.
+});
+
+$("#dedup-btn").on("click", function() {
+    console.log(access_token);
+
+    var playlist_link = $("#dedup-txtbox").val();
+
+    alert(playlist_link);
+
+    // Check if playlist is correct $.ajax... etc.
+});
 
 
 // $(function () {
