9bf8a3808683ef58756690272b5983e544322396
Hayes Choy <hayeselnut2127@gmail.com>
2021-09-13 13:08:37 +1000

    hayes show case draft done, with socials

---
+++ src/components/showcases/hayes-showcase.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import { Header, Icon } from 'semantic-ui-react';
+
+import { bodyStyle } from '../../helpers/body-style';
+
+const HayesShowcase = () => {
+  return (
+    <>
+      <Helmet>
+        <style type="text/css">{bodyStyle('white')}</style>
+      </Helmet>
+
+      <Header as='h1' content='Hayeselnut' />
+      <Header as='h2' content='Software Engineer' />
+      <p>
+        UNSW Co-op Scholar (HD WAM)
+      </p>
+      <p>
+        Interned at Atlassian as Data Platform Engineer
+      </p>
+
+      <a href='https://www.linkedin.com/in/hayes-choy-293954153/' target='_blank' rel="noreferrer">
+        <Icon name='linkedin' size='large' circular className='socials-icon' />
+      </a>
+
+      <a href='https://github.com/hayeselnut/' target='_blank' rel="noreferrer">
+        <Icon name='github' size='large' circular className='socials-icon' />
+      </a>
+    </>
+  );
+};
+
+export default HayesShowcase;
